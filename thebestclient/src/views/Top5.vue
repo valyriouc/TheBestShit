2a3b2f8439cbd8939b90221cc41561040f7cc5ce
Valacor <valyrious.informatics@gmail.com>
2025-10-03 22:26:55 +0200

    adding a hacker/science like css framework

--- thebestclient/src/views/Top5.vue
+++ thebestclient/src/views/Top5.vue
@@ -1,9 +1,11 @@
 <script setup>
+    import { useRoute } from 'vue-router';
 
+    const route = useRoute().params.category;
 </script>
 
 <template>
-    <h1>Top 5 {{ category }}</h1>
+    <h1>Top 5 - {{ route}}</h1>
 </template>
 <style scoped>
 </style>
